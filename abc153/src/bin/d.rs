082f380d2086243fc3b09794aa7e3e9d37e21678
hkdnet <satoko.itse@gmail.com>
2020-05-28 03:55:01 +0000

    d

--- abc153/src/bin/d.rs
+++ abc153/src/bin/d.rs
@@ -1,3 +1,11 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input!(h: u128);
+    let mut n = 1;
+    while h >= 2u128.pow(n) {
+        n += 1;
+    }
+
+    println!("{}", 2u128.pow(n) - 1);
 }
