ab0cee8f6d25948fe682e175fa4c14edb8be2863
hkdnet <satoko.itse@gmail.com>
2020-06-05 14:46:25 +0000

    a

--- abc159/src/bin/a.rs
+++ abc159/src/bin/a.rs
@@ -1,3 +1,12 @@
+use proconio::input;
 fn main() {
-    unimplemented!();
+    input!(n: usize, m: usize);
+    let mut ans = 0;
+    if n > 1 {
+        ans += n * (n - 1) / 2;
+    }
+    if m > 1 {
+        ans += m * (m - 1) / 2;
+    }
+    println!("{}", ans);
 }
