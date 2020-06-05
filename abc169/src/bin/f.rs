76562044dbb0daec8071401832748e2e1f80b117
hkdnet <satoko.itse@gmail.com>
2020-06-05 14:33:44 +0000

    dp sitaikedo

--- abc169/src/bin/f.rs
+++ abc169/src/bin/f.rs
@@ -1,3 +1,12 @@
+use proconio::input;
+use std::collections::HashMap;
+
 fn main() {
-    unimplemented!();
+    input!(n: u32, s: u32);
+    input!(aa: [u32; n]);
+    let mut used = HashMap::new();
+    for i in 0..aa.len() {
+        used.insert(i, false);
+    }
+    println!("{}", s)
 }
