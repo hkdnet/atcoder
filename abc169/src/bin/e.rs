b846e05d41bdeecc14e71c0c8a9ac9a61dd918b4
hkdnet <satoko.itse@gmail.com>
2020-06-05 14:28:49 +0000

    e?

--- abc169/src/bin/e.rs
+++ abc169/src/bin/e.rs
@@ -1,3 +1,28 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input!(n: usize);
+    input!(qq: [(u64, u64); n]);
+    let mut aa = vec![0; n];
+    let mut bb = vec![0; n];
+    for (i, (a, b)) in qq.iter().enumerate() {
+        aa[i] = *a;
+        bb[i] = *b;
+    }
+    aa.sort();
+    bb.sort();
+
+    let ans = if n % 2 == 0 {
+        let idx = n / 2 - 1;
+        let a = (aa[idx] + aa[idx + 1]) * 5;
+        let b = (bb[idx] + bb[idx + 1]) * 5;
+        (b - a) / 5 + 1
+    } else {
+        let idx = n / 2;
+        let a = aa[idx];
+        let b = bb[idx];
+        b - a + 1
+    };
+
+    println!("{}", ans);
 }
