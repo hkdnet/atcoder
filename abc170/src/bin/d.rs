8e93fd331337c54896be6dae0ae53e080308d22e
hkdnet <satoko.itse@gmail.com>
2020-06-10 13:37:49 +0000

    Follow tanakh

--- abc170/src/bin/d.rs
+++ abc170/src/bin/d.rs
@@ -1,47 +1,39 @@
+use itertools::Itertools;
 use proconio::input;
-use std::collections::{HashMap, HashSet};
-
-fn gcd(a: u32, b: u32) -> u32 {
-    if b == 0 {
-        a
-    } else {
-        gcd(b, a % b)
-    }
-}
+use std::collections::BTreeSet;
 
 fn main() {
-    input!(n: usize); // 1<= n <= 2* 10**5
-    input!(mut aa: [u32; n]);
+    input! {
+        n: usize,
+        a: [i64; n],
+    }
 
-    aa.sort();
-    let mut cnt = HashMap::new();
+    let mut mm = BTreeSet::new();
+    let mut mult = BTreeSet::new();
 
-    let mut g = aa[0];
-    for a in aa.iter() {
-        cnt.entry(a).and_modify(|e| *e += 1).or_insert(1);
-        g = gcd(g, *a);
+    for a in a {
+        if mm.contains(&a) {
+            mult.insert(a);
+        } else {
+            mm.insert(a);
+        }
     }
 
     let mut ans = 0;
-    let mut divider = HashSet::new();
-    for i in 0..n {
-        let a = aa[i] / g;
 
-        if *cnt.get(&aa[i]).unwrap() > 1 {
-            divider.insert(a);
+    let v = mm.iter().cloned().collect_vec();
+    for x in v {
+        if !mm.contains(&x) {
             continue;
         }
 
-        let mut ok = true;
-        for e in divider.iter() {
-            if a % e == 0 {
-                ok = false;
-                break;
-            }
-        }
-        if ok {
+        if !mult.contains(&x) {
             ans += 1;
-            divider.insert(a);
+        }
+        let mut y = x;
+        while y <= 1_000_000 {
+            mm.remove(&y);
+            y += x;
         }
     }
 
