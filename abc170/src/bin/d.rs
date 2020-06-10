b093eef82f0fa6ab8d4a008160e3d40fe8d318b1
hkdnet <satoko.itse@gmail.com>
2020-06-10 06:53:53 +0000

    ＼(^o^)／

--- abc170/src/bin/d.rs
+++ abc170/src/bin/d.rs
@@ -1,33 +1,47 @@
 use proconio::input;
 use std::collections::{HashMap, HashSet};
+
+fn gcd(a: u32, b: u32) -> u32 {
+    if b == 0 {
+        a
+    } else {
+        gcd(b, a % b)
+    }
+}
+
 fn main() {
     input!(n: usize); // 1<= n <= 2* 10**5
-    input!(mut aa:  [u32; n]);
+    input!(mut aa: [u32; n]);
 
     aa.sort();
     let mut cnt = HashMap::new();
 
+    let mut g = aa[0];
     for a in aa.iter() {
         cnt.entry(a).and_modify(|e| *e += 1).or_insert(1);
+        g = gcd(g, *a);
     }
+
     let mut ans = 0;
     let mut divider = HashSet::new();
     for i in 0..n {
+        let a = aa[i] / g;
+
         if *cnt.get(&aa[i]).unwrap() > 1 {
-            divider.insert(aa[i]);
+            divider.insert(a);
             continue;
         }
 
         let mut ok = true;
         for e in divider.iter() {
-            if aa[i] % e == 0 {
+            if a % e == 0 {
                 ok = false;
                 break;
             }
         }
         if ok {
             ans += 1;
-            divider.insert(aa[i]);
+            divider.insert(a);
         }
     }
 
--- abc170/src/bin/e.rs
+++ abc170/src/bin/e.rs
@@ -1,49 +1,28 @@
 use proconio::input;
+use std::collections::BinaryHeap;
 
-struct U {
-    n: usize,
-    rates: Vec<u32>,
-    children: Vec<u32>,
-}
-
-impl U {
-    fn new(n: usize, rates: Vec<u32>, children: Vec<u32>) -> U {
-        U {
-            n: n,
-            rates: rates,
-            children: children,
-        }
-    }
-    fn union(self: &mut Self, child: u32, k: u32) {
-        unimplemented!();
-    }
-    fn min(self: &mut Self) -> u32 {
-        let mut m = 10000000000;
-        for i in 0..self.n {
-            let s = self.rates[self.root(i) as usize];
-            if s < m {
-                m = s;
-            }
-        }
-        return m;
-    }
-    fn root(self: &mut Self, idx: usize) -> u32 {}
-}
+const K: usize = 2000000;
 
 fn main() {
     input!(n: usize, q: usize);
-    input!(abs: [(u32, u32); n]);
-    input!(cds: [(usize, u32); q]);
+    input!(abs: [(u32, usize); n]);
+    input!(cds: [(usize, usize); q]);
+    let mut heaps = vec![BinaryHeap::new(); K];
 
     let mut rates = vec![0; n];
     let mut children = vec![0; n];
     for i in 0..n {
         let (r, k) = abs[i];
         rates[i] = r;
+        heaps[k].push(r);
         children[i] = k;
     }
 
-    let u = U::new(n, rates, children);
+    for (c, d) in cds {
+        let now = children[c];
+        children[c] = d;
+        heaps[now].retain(|e| e != rates[c]);
+    }
 
     println!("{:?}", abs);
     println!("{:?}", cds);
