97e475275390a5e9c43c7b2d83f49876db6f5b24
hkdnet <satoko.itse@gmail.com>
2020-06-10 06:13:55 +0000

    tmp

--- abc170/src/bin/d.rs
+++ abc170/src/bin/d.rs
@@ -1,5 +1,5 @@
 use proconio::input;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 fn main() {
     input!(n: usize); // 1<= n <= 2* 10**5
     input!(mut aa:  [u32; n]);
@@ -11,20 +11,23 @@ fn main() {
         cnt.entry(a).and_modify(|e| *e += 1).or_insert(1);
     }
     let mut ans = 0;
+    let mut divider = HashSet::new();
     for i in 0..n {
         if *cnt.get(&aa[i]).unwrap() > 1 {
+            divider.insert(aa[i]);
             continue;
         }
 
         let mut ok = true;
-        for j in 0..i {
-            if aa[i] % aa[j] == 0 {
+        for e in divider.iter() {
+            if aa[i] % e == 0 {
                 ok = false;
                 break;
             }
         }
         if ok {
             ans += 1;
+            divider.insert(aa[i]);
         }
     }
 
