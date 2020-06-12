d70bcdd08a8a69b59d165aee579b5a33a3787d1e
hkdnet <satoko.itse@gmail.com>
2020-06-12 04:42:27 +0000

    d

--- abc154/src/bin/d.rs
+++ abc154/src/bin/d.rs
@@ -1,3 +1,22 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input!(n: usize, k: usize);
+    input!(pp: [usize; n]);
+
+    let mut sum = vec![0; n + 1];
+    sum[0] = pp[0];
+    for i in 0..n {
+        sum[i + 1] = sum[i] + pp[i];
+    }
+    let mut max = 0;
+    for i in 0..(n - k + 1) {
+        let tmp = sum[i + k] - sum[i];
+        if tmp > max {
+            max = tmp;
+        }
+    }
+
+    let tmp = max + k;
+    println!("{}", tmp as f32 / 2f32);
 }
