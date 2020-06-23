d0fe60f8b8910888263e9500f0ddbd780c2e59ae
hkdnet <satoko.itse@gmail.com>
2020-06-23 17:48:46 +0000

    C

--- abc156/src/bin/c.rs
+++ abc156/src/bin/c.rs
@@ -1,3 +1,18 @@
 fn main() {
-    unimplemented!();
+    use proconio::input;
+    input!(n: usize);
+    input!(xx: [i32; n]);
+
+    let loss = |p: i32| -> i32 {
+        let mut ret = 0;
+        for x in xx.iter() {
+            ret += (p - x) * (p - x);
+        }
+        ret
+    };
+
+    let sum = xx.iter().fold(0, |a, e| a + e);
+    let m = sum / n as i32;
+    let l = std::cmp::min(loss(m), loss(m + 1));
+    println!("{}", l);
 }
