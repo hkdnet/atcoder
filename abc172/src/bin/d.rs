47f6398cb5c002847c6eef897b3bef982c8a650f
hkdnet <satoko.itse@gmail.com>
2020-06-27 15:03:49 +0000

    d

--- abc172/src/bin/d.rs
+++ abc172/src/bin/d.rs
@@ -1,3 +1,13 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input!(n: u64);
+    let mut ans = 0;
+    for j in 0..n {
+        let j = j + 1; // factor
+        for i in (j..n + 1).step_by(j as usize) {
+            ans += i;
+        }
+    }
+    println!("{}", ans);
 }
