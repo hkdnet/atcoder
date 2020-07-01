a42d33535cc2eb03e57b14cdf8f8a73ce48631df
hkdnet <satoko.itse@gmail.com>
2020-07-01 10:01:47 +0000

    D

--- abc173/src/bin/d.rs
+++ abc173/src/bin/d.rs
@@ -1,3 +1,24 @@
 fn main() {
-    unimplemented!()
+    use proconio::input;
+    input!(n: usize);
+    input!(mut aa: [u64; n]);
+
+    aa.sort();
+    aa.reverse();
+
+    let mut ans = aa[0];
+    let mut tmp = n - 2;
+    let mut idx = 1;
+    while tmp > 0 {
+        ans += aa[idx];
+        tmp -= 1;
+        if tmp == 0 {
+            break;
+        }
+        ans += aa[idx];
+        tmp -= 1;
+        idx += 1;
+    }
+
+    println!("{}", ans);
 }
