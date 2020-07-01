300f6db2668afde5b22acc56d0a32f399fd815d3
hkdnet <satoko.itse@gmail.com>
2020-07-01 07:57:49 +0000

    primes

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::BTreeSet;
+
 pub fn binary_search<T, F>(l: T, r: T, f: F) -> T
 where
   T: std::ops::Add<Output = T> + std::ops::Div<Output = T> + PartialEq + From<u8> + Copy,
@@ -21,11 +23,40 @@ where
 
   l
 }
+
+pub fn primes<T>(n: T) -> BTreeSet<T>
+where
+  T: PartialEq
+    + From<u8>
+    + Copy
+    + std::cmp::Ord
+    + std::ops::Mul<Output = T>
+    + std::ops::Add<Output = T>
+    + std::ops::Div<Output = T>
+    + std::ops::Rem<Output = T>,
+{
+  let mut m = n;
+  let mut p = T::from(2u8);
+  let mut ret = BTreeSet::new();
+  while p * p <= n {
+    while (m % p) == T::from(0u8) {
+      ret.insert(p);
+      m = m / p;
+    }
+    p = p + T::from(1u8);
+  }
+  if m != T::from(1u8) {
+    ret.insert(m);
+  }
+
+  ret
+}
+
 #[cfg(test)]
 mod tests {
-  use super::binary_search;
+  use super::*;
   #[test]
-  fn it_works() {
+  fn test_binary_search() {
     let arr = &vec![1, 2, 3, 4];
     assert_eq!(binary_search(0, arr.len(), |i| arr[i] < 3), 1);
     assert_eq!(binary_search(0, arr.len(), |i| arr[i] < 100), 3);
@@ -34,5 +65,25 @@ mod tests {
     assert_eq!(binary_search(1, 3, |i| arr[i] < 100), 2);
     assert_eq!(binary_search(1, 3, |i| arr[i] < -1), 1);
     assert_eq!(binary_search(4, -1, |i| arr[i as usize] < 3), 0);
+  }
+
+  #[test]
+  fn test_primes() {
+    use std::iter::FromIterator;
+    let examples = vec![
+      (1, vec![]),
+      (2, vec![2]),
+      (4, vec![2]),
+      (6, vec![2, 3]),
+      (11, vec![11]),
+    ];
+    for (n, v) in examples {
+      let actual = primes(n);
+      let expected = BTreeSet::from_iter(v);
+      assert_eq!(actual, expected);
+    }
+    assert_eq!(primes(1 as usize), BTreeSet::new());
+    assert_eq!(primes(1 as i32), BTreeSet::new());
+    assert_eq!(primes(1 as u32), BTreeSet::new());
   }
 }
