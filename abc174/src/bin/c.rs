7f703e40a4865439f9296fd2454af432ed81789b
hkdnet <satoko.itse@gmail.com>
2020-08-05 04:32:40 +0000

    C AC

--- abc174/src/bin/c.rs
+++ abc174/src/bin/c.rs
@@ -1,3 +1,22 @@
 fn main() {
-    unimplemented!();
+    use proconio::input;
+    input!(k: u64);
+
+    use std::collections::BTreeSet;
+    let mut delta = 7 % k;
+    let mut m = 7 % k;
+    let mut used: BTreeSet<u64> = BTreeSet::new();
+
+    while m != 0 {
+        delta *= 10;
+        delta %= k;
+        m += delta;
+        m %= k;
+        if used.contains(&m) {
+            println!("-1");
+            return;
+        }
+        used.insert(m);
+    }
+    println!("{}", used.len() + 1);
 }
