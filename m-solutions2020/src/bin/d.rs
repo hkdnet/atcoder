d2acf5a4b45c5242cfb47665dc2b9e71da5565a8
hkdnet <satoko.itse@gmail.com>
2020-08-05 12:20:28 +0000

    TLE: D

--- m-solutions2020/src/bin/d.rs
+++ m-solutions2020/src/bin/d.rs
@@ -1,3 +1,56 @@
 fn main() {
-    unimplemented!();
+    use proconio::input;
+    use std::collections::BTreeMap;
+
+    input!(n: usize);
+    input!(aa: [u32; n]);
+
+    let mut dp: Vec<BTreeMap<u32, u32>> = Vec::new();
+    {
+        let mut zero = BTreeMap::new();
+        zero.insert(1000, 0);
+        dp.push(zero);
+    }
+
+    for (i, a) in aa.iter().enumerate() {
+        let a = *a;
+        let t = &dp[i];
+        let mut n = BTreeMap::new();
+        for (m, s) in t.iter() {
+            let m = *m;
+            let s = *s;
+            n.entry(m)
+                .and_modify(|e| *e = std::cmp::max(*e, s))
+                .or_insert(s);
+            {
+                // 買う
+                let mut m = m;
+                let mut s = s;
+                while m >= a {
+                    m -= a;
+                    s += 1;
+                    n.entry(m)
+                        .and_modify(|e| *e = std::cmp::max(*e, s))
+                        .or_insert(s);
+                }
+            }
+            {
+                // 売る
+                let mut m = m;
+                let mut s = s;
+                while s > 0 {
+                    m += a;
+                    s -= 1;
+                    n.entry(m)
+                        .and_modify(|e| *e = std::cmp::max(*e, s))
+                        .or_insert(s);
+                }
+            }
+        }
+        dp.push(n);
+    }
+    let t = dp.last().unwrap();
+
+    let (max, _) = t.iter().last().unwrap();
+    println!("{}", max);
 }
