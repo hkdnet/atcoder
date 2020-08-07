d3e865b9c9605e5098b0ad79efaf2509c9217e6d
hkdnet <satoko.itse@gmail.com>
2020-08-07 23:06:02 +0000

    a, b

--- abc175/src/bin/b.rs
+++ abc175/src/bin/b.rs
@@ -1,3 +1,27 @@
 fn main() {
-    unimplemented!();
+    use proconio::input;
+    input!(n: usize);
+    input!(mut ll: [usize; n]);
+
+    ll.sort();
+
+    let mut cnt = 0;
+
+    for i in 0..n {
+        for j in i + 1..n {
+            for k in j + 1..n {
+                if ll[i] == ll[j] {
+                    continue;
+                }
+                if ll[j] == ll[k] {
+                    continue;
+                }
+                if ll[i] + ll[j] > ll[k] {
+                    cnt += 1;
+                }
+            }
+        }
+    }
+
+    println!("{}", cnt);
 }
