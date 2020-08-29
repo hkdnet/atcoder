c589e3627441ef96cc0895db6773f7b7dbf86fdb
hkdnet <satoko.itse@gmail.com>
2020-08-29 13:09:50 +0000

    D AC , E WAx1

--- abc177/src/bin/d.rs
+++ abc177/src/bin/d.rs
@@ -1,6 +1,51 @@
 #![allow(unused_imports)]
 use proconio::input;
 use proconio::marker::*;
+use std::collections::BTreeMap;
+
+pub struct UnionFind(Vec<usize>);
+
+impl UnionFind {
+    pub fn new(n: usize) -> UnionFind {
+        UnionFind((0..n).collect())
+    }
+
+    pub fn find(&mut self, i: usize) -> usize {
+        if self.0[i] == i {
+            i
+        } else {
+            let p = self.0[i];
+            self.0[i] = self.find(p);
+            self.0[i]
+        }
+    }
+
+    pub fn union(&mut self, i: usize, j: usize) {
+        let ni = self.find(i);
+        let nj = self.find(j);
+        self.0[ni] = nj;
+    }
+}
 fn main() {
-    unimplemented!();
+    input!(n: usize, m: usize);
+    let mut uf = UnionFind::new(n);
+    for _ in 0..m {
+        input!(a: Usize1, b: Usize1);
+        uf.union(a, b);
+    }
+    let mut cnt: BTreeMap<usize, usize> = BTreeMap::new();
+    // flatten
+    for i in 0..n {
+        uf.find(i);
+    }
+    for i in 0..n {
+        let r = uf.find(i);
+        cnt.entry(r).and_modify(|e| *e += 1).or_insert(1);
+    }
+    let mut max = 0;
+    for (_, &tmp) in cnt.iter() {
+        max = std::cmp::max(tmp, max);
+    }
+
+    println!("{}", max);
 }
--- abc177/src/bin/e.rs
+++ abc177/src/bin/e.rs
@@ -1,6 +1,101 @@
 #![allow(unused_imports)]
 use proconio::input;
 use proconio::marker::*;
+
+use std::collections::BTreeSet;
+
+fn primes_upto(n: u32) -> BTreeSet<u32> {
+    let nu = n as usize;
+    let mut v = vec![true; nu + 1];
+
+    for i in 2..(nu / 2) {
+        if v[i] {
+            let mut idx = i * 2;
+            while idx <= nu {
+                v[idx] = false;
+
+                idx += i;
+            }
+        }
+    }
+    v[0] = false;
+    v[1] = false;
+    let mut ret = BTreeSet::new();
+    for (i, &b) in v.iter().enumerate() {
+        if b {
+            ret.insert(i as u32);
+        }
+    }
+
+    ret
+}
+
+fn gcd(a: u32, b: u32) -> u32 {
+    if b == 0 {
+        a
+    } else {
+        gcd(b, a % b)
+    }
+}
+fn setgcd(arr: &Vec<u32>) -> u32 {
+    let n = arr.len();
+    if n == 1 {
+        return arr[0];
+    }
+    let l = n / 2;
+    let mut v = vec![0; if n % 2 == 0 { l } else { l + 1 }];
+    for i in 0..l {
+        let idx = 2 * i;
+        let g = gcd(arr[idx], arr[idx + 1]);
+        v[i] = g;
+    }
+    if n % 2 != 0 {
+        v[l] = arr[n - 1];
+    }
+    setgcd(&v)
+}
 fn main() {
-    unimplemented!();
+    use proconio::input;
+    input!(n: usize);
+    input!(aa: [u32; n]);
+
+    let max = 1000000;
+    if setgcd(&aa) == 1 {
+        let ps = primes_upto(max);
+        // println!("{:?}", ps);
+        let mut used = BTreeSet::new();
+        for a in aa {
+            if ps.contains(&a) {
+                used.insert(a);
+                continue;
+            }
+            let mut a = a;
+            for &p in ps.iter() {
+                if a < p {
+                    break;
+                }
+                if a % p == 0 {
+                    if used.contains(&p) {
+                        break;
+                    }
+                    used.insert(p);
+                    while a % p == 0 {
+                        a /= p;
+                    }
+                    if a == 1 {
+                        break;
+                    }
+                }
+            }
+            // println!("ps = {:?}", ps);
+            // println!("a = {}", a);
+            if a != 1 {
+                println!("setwise coprime");
+                return;
+            }
+        }
+        println!("pairwise coprime");
+    } else {
+        println!("not coprime");
+    }
 }
