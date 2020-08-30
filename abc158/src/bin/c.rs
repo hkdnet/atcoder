0e383722a89c498820940779f89dcc2d376ea401
hkdnet <satoko.itse@gmail.com>
2020-08-30 03:35:01 +0000

    C

--- abc158/src/bin/c.rs
+++ abc158/src/bin/c.rs
@@ -2,5 +2,14 @@
 use proconio::input;
 use proconio::marker::*;
 fn main() {
-    unimplemented!();
+    input!(a: u32, b: u32);
+    for n in 0..10000 {
+        let aa = (n as f64 * 0.08f64).floor() as u32;
+        let bb = (n as f64 * 0.10f64).floor() as u32;
+        if aa == a && bb == b {
+            println!("{}", n);
+            return;
+        }
+    }
+    println!("{}", -1);
 }
