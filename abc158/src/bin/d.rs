3857ec0dc3240e610e38f07219ccba1b1179455d
hkdnet <satoko.itse@gmail.com>
2020-08-30 03:43:37 +0000

    d

--- abc158/src/bin/d.rs
+++ abc158/src/bin/d.rs
@@ -1,6 +1,38 @@
 #![allow(unused_imports)]
 use proconio::input;
 use proconio::marker::*;
+use std::collections::VecDeque;
 fn main() {
-    unimplemented!();
+    input!(cc: Chars, q: usize);
+    let mut d = VecDeque::new();
+    for c in cc {
+        d.push_back(c);
+    }
+
+    let mut reversed = false;
+
+    for _ in 0..q {
+        input!(ty: usize);
+        if ty == 1 {
+            reversed = !reversed;
+        } else {
+            input!(f: usize, c: char);
+            let is_front = f == 1;
+            match (is_front, reversed) {
+                (true, false) | (false, true) => {
+                    // 前から
+                    d.push_front(c);
+                }
+                _ => {
+                    d.push_back(c);
+                }
+            }
+        }
+    }
+    let output = if reversed {
+        d.iter().rev().collect::<String>()
+    } else {
+        d.iter().collect::<String>()
+    };
+    println!("{}", output);
 }
