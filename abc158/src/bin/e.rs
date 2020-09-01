175bd747f6a9f45d61abf680078bd2c428e0f08e
hkdnet <satoko.itse@gmail.com>
2020-09-01 06:14:06 +0000

    simplify

--- abc158/src/bin/e.rs
+++ abc158/src/bin/e.rs
@@ -48,49 +48,17 @@ fn main() {
         return;
     }
 
-    let modinv = |aa: i32, m: i32| -> i32 {
-        let mut a = aa;
-        let mut b = m;
-        let mut u = 1;
-        let mut v = 0;
-        while b != 0 {
-            let t = a / b;
-            a -= t * b;
-            std::mem::swap(&mut a, &mut b);
-
-            u -= t * v;
-            std::mem::swap(&mut u, &mut v);
-        }
-        u %= m;
-        if u < 0 {
-            u += m;
-        }
-        u
-    };
-
     let acc = build_acc(&nn, p);
-    // println!("{:?}", acc);
 
     let mut cnt = 0u64;
-    let mut tmp = 1;
-    for _ in 0..n - 1 {
-        tmp *= 10;
-        tmp %= p;
-    }
     for r in 0..n {
         let r = r + 1;
         for l in 0..r {
             let s = acc[l] - acc[r];
-            let s = s % p;
-            let s = s * modinv(tmp, p);
-            let s = s % p;
-            // println!("for [{}, {}) -> {}", l, r, s);
-            if s == 0 {
+            if s % p == 0 {
                 cnt += 1;
             }
         }
-        tmp *= modinv(10, p);
-        tmp %= p;
     }
 
     println!("{}", cnt);
