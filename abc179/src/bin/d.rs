3342532edc4cbb8932d30d2fc45b6268e4e536ab
hkdnet <satoko.itse@gmail.com>
2020-10-03 15:52:46 +0000

    D AC

--- abc179/src/bin/d.rs
+++ abc179/src/bin/d.rs
@@ -2,60 +2,40 @@
 use proconio::input;
 use proconio::marker::*;
 
-const MOD: u32 = 998244353;
+const MOD: u64 = 998244353;
 
 fn main() {
     input!(n: usize, k: usize);
-    let mut v = vec![];
-    for _ in 0..k {
-        input!(l: usize, r: usize);
-        let d = r - l;
-        v.push((l, d));
-    }
-
-    use std::collections::BTreeMap;
-    let mut dp: Vec<BTreeMap<usize, u32>> = vec![BTreeMap::new(); n + 1];
-    dp[1].insert(0, 1);
-
-    for i in 1..n {
-        if dp[i].len() == 0 {
-            continue;
+    let allowed: Vec<(usize, usize)> = {
+        let mut tmp = std::collections::BTreeSet::new();
+        for _ in 0..k {
+            input!(l: usize, r: usize);
+            tmp.insert((l, r));
         }
-        for &(l, d) in v.iter() {
-            let idx = i + l;
-            if idx > n {
-                break;
-            }
-            let mut m = dp[idx].clone();
-            for (k, &v) in dp[i].iter() {
-                let kk = k + d;
-                match dp[idx].get(&kk) {
-                    Some(a) => {
-                        m.insert(kk, v + a);
-                    }
-                    None => {
-                        m.insert(kk, v);
-                    }
-                }
-            }
+        tmp.into_iter().collect()
+    };
 
-            dp[idx] = m;
+    let mut dp = vec![0; n];
+    dp[0] = 1;
+    dp[1] = MOD - 1;
+    for i in 0..n {
+        // println!("i = {}", i);
+        if i != 0 {
+            dp[i] += dp[i - 1];
+            dp[i] %= MOD;
+        }
+        for &(l, r) in allowed.iter() {
+            if i + l < n {
+                dp[i + l] += dp[i];
+                dp[i + l] %= MOD;
+            }
+            if i + r + 1 < n {
+                dp[i + r + 1] += MOD - dp[i];
+                dp[i + r + 1] %= MOD;
+            }
+            // println!("{:?}", dp);
         }
     }
 
-    println!("{:?}", dp);
-    let mut cnt = 0;
-    for i in 0..n + 1 {
-        let m = &dp[i];
-        if m.len() == 0 {
-            continue;
-        }
-        for (k, v) in m.iter() {
-            if i + k >= n {
-                cnt += v;
-                cnt %= MOD;
-            }
-        }
-    }
-    println!("{}", cnt);
+    println!("{}", dp[n - 1]);
 }
