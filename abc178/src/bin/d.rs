5b26bdde541312f248d89513be69d875b44f21d7
hkdnet <satoko.itse@gmail.com>
2020-10-03 16:18:23 +0000

    178D AC

--- abc178/src/bin/d.rs
+++ abc178/src/bin/d.rs
@@ -1,6 +1,34 @@
 #![allow(unused_imports)]
 use proconio::input;
-use proconio::marker::*;
+
+const MOD: u64 = 1000000007;
+
 fn main() {
-    unimplemented!();
+    input!(s: usize);
+    let mut dp = vec![0; s + 1];
+    dp[0] = 1;
+
+    for idx in 3..s + 1 {
+        dp[idx] = dp[idx - 1] + dp[idx - 3];
+        dp[idx] %= MOD;
+    }
+
+    println!("{}", dp[s]);
 }
+
+// 3 -> 1
+// 4 -> 1
+// 5 -> 1
+// 6 -> 2
+// 1 6
+// 1 3, 3
+// 7 -> 3
+// 2 3, 4 | 4, 3
+// 1 7
+// 8 -> 4
+// 2 3, 5 | 5, 3
+// 1 4, 4
+// 1 8
+// 9 -> 4
+// 3 3, 6 | 6, 3 | 3, 3, 3
+// 1 4, 5
