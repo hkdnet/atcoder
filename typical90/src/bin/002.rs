e022b48bf28ec75357504a4f1d6c14d4aba13a1d
hkdnet <satoko.itse@gmail.com>
2021-07-19 03:40:15 +0000

    002

--- typical90/src/bin/002.rs
+++ typical90/src/bin/002.rs
@@ -2,6 +2,31 @@
 use proconio::input;
 use proconio::marker::*;
 
+use std::collections::BTreeSet;
+
+fn f(n: usize) -> Vec<String> {
+    let mut tmp = vec![("".to_string(), 0)];
+    for _ in 0..n {
+        let t = tmp.iter().flat_map(|(s, cnt)| {
+            let mut v = vec![(format!("{}(", s), cnt + 1)];
+            if *cnt != 0 {
+                v.push((format!("{})", s), cnt - 1));
+            }
+            v
+        });
+        tmp = t.collect();
+    }
+    tmp.into_iter()
+        .filter_map(|(s, c)| if c == 0 { Some(s) } else { None })
+        .collect()
+}
 fn main() {
-    unimplemented!();
+    input!(n: usize);
+    if n % 2 != 0 {
+        return;
+    }
+
+    for s in f(n) {
+        println!("{}", s)
+    }
 }
