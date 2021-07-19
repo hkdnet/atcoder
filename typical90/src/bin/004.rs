b191e667993d2c9347a332d6fdbda1e351f62f9d
hkdnet <satoko.itse@gmail.com>
2021-07-19 10:33:51 +0000

    004

--- typical90/src/bin/004.rs
+++ typical90/src/bin/004.rs
@@ -3,5 +3,33 @@ use proconio::input;
 use proconio::marker::*;
 
 fn main() {
-    unimplemented!();
+    input!(h: usize, w: usize);
+    input!(m: [[i32; w]; h]);
+
+    let mut w_sum = vec![0; h];
+    let mut h_sum = vec![0; w];
+    for (idx, ws) in m.iter().enumerate() {
+        w_sum[idx] = ws.iter().sum();
+    }
+    for i in 0..w {
+        let mut sum = 0;
+        for j in 0..h {
+            sum += m[j][i];
+        }
+        h_sum[i] = sum;
+    }
+
+    for i in 0..h {
+        let mut v = vec![0; w];
+        for j in 0..w {
+            v[j] = h_sum[j] + w_sum[i] - m[i][j]
+        }
+        println!(
+            "{}",
+            v.into_iter()
+                .map(|e| e.to_string())
+                .collect::<Vec<String>>()
+                .join(" ")
+        );
+    }
 }
