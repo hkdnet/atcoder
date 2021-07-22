896f586fe15dbab13af23b448a8e343be35dd289
hkdnet <satoko.itse@gmail.com>
2021-07-22 15:05:28 +0000

    006 007 AC

--- typical90/src/bin/006.rs
+++ typical90/src/bin/006.rs
@@ -3,5 +3,39 @@ use proconio::input;
 use proconio::marker::*;
 
 fn main() {
-    unimplemented!();
+    input!(n: usize, k: usize, cs: Chars);
+    let indexer = |c: char| -> usize {
+        let idx = c.to_digit(36).unwrap();
+        (idx - 10) as usize
+    };
+    let mut ans = vec![];
+    let mut l = 0;
+    while ans.len() != k {
+        let mut tmp = [None; 26];
+        let r = n + ans.len() - k;
+        let mut c_idx = l;
+        // println!("l = {}, r = {}", l, r); // debug
+        while c_idx <= r {
+            let idx = indexer(cs[c_idx]);
+            if tmp[idx] == None {
+                tmp[idx] = Some(c_idx);
+            }
+            c_idx += 1;
+        }
+        // println!("{:?}", tmp); // debug
+        for &e in tmp.iter() {
+            if let Some(idx) = e {
+                ans.push(cs[idx]);
+                l = idx + 1;
+                break;
+            }
+        }
+    }
+    println!(
+        "{}",
+        ans.into_iter()
+            .map(|c| c.to_string())
+            .collect::<Vec<_>>()
+            .join("")
+    )
 }
--- typical90/src/bin/007.rs
+++ typical90/src/bin/007.rs
@@ -2,6 +2,31 @@
 use proconio::input;
 use proconio::marker::*;
 
+use comp::binary_search::binary_search;
+
 fn main() {
-    unimplemented!();
+    input!(n: usize, classes: [i32; n], q: usize, rates: [i32; q]);
+    let mut classes = classes;
+    classes.sort_unstable();
+
+    if classes.len() == 1 {
+        let c = classes[0];
+        for r in rates {
+            let ans = (r - c).abs();
+            println!("{}", ans);
+        }
+    } else {
+        for r in rates {
+            let idx = binary_search(0, n, |idx| classes[idx] < r);
+            let ans = if idx == 0 {
+                std::cmp::min((classes[0] - r).abs(), (classes[1] - r).abs())
+            } else if idx == n - 1 {
+                std::cmp::min((classes[n - 1] - r).abs(), (classes[n - 2] - r).abs())
+            } else {
+                std::cmp::min((classes[idx] - r).abs(), (classes[idx + 1] - r).abs())
+            };
+
+            println!("{}", ans);
+        }
+    }
 }
