fac156d48973d26bd477706a6ffde924212e7a49
hkdnet <satoko.itse@gmail.com>
2021-07-22 15:38:34 +0000

    008 010 AC

--- typical90/src/bin/008.rs
+++ typical90/src/bin/008.rs
@@ -2,6 +2,80 @@
 use proconio::input;
 use proconio::marker::*;
 
+use std::collections::BTreeMap;
+
+const MOD: i64 = 1_000_000_007;
+
 fn main() {
-    unimplemented!();
+    input!(_: usize, cs: Chars);
+    let mut m = BTreeMap::new();
+    for c in cs {
+        match c {
+            'a' => {
+                m.entry('a')
+                    .and_modify(|c| {
+                        *c += 1;
+                        *c %= MOD;
+                    })
+                    .or_insert(1);
+            }
+            't' => {
+                let &cnt = m.get(&'a').unwrap_or(&0);
+                m.entry('t')
+                    .and_modify(|c| {
+                        *c += cnt;
+                        *c %= MOD;
+                    })
+                    .or_insert(cnt);
+            }
+            'c' => {
+                let &cnt = m.get(&'t').unwrap_or(&0);
+                m.entry('c')
+                    .and_modify(|c| {
+                        *c += cnt;
+                        *c %= MOD;
+                    })
+                    .or_insert(cnt);
+            }
+            'o' => {
+                let &cnt = m.get(&'c').unwrap_or(&0);
+                m.entry('o')
+                    .and_modify(|c| {
+                        *c += cnt;
+                        *c %= MOD;
+                    })
+                    .or_insert(cnt);
+            }
+            'd' => {
+                let &cnt = m.get(&'o').unwrap_or(&0);
+                m.entry('d')
+                    .and_modify(|c| {
+                        *c += cnt;
+                        *c %= MOD;
+                    })
+                    .or_insert(cnt);
+            }
+            'e' => {
+                let &cnt = m.get(&'d').unwrap_or(&0);
+                m.entry('e')
+                    .and_modify(|c| {
+                        *c += cnt;
+                        *c %= MOD;
+                    })
+                    .or_insert(cnt);
+            }
+            'r' => {
+                let &cnt = m.get(&'e').unwrap_or(&0);
+                m.entry('r')
+                    .and_modify(|c| {
+                        *c += cnt;
+                        *c %= MOD;
+                    })
+                    .or_insert(cnt);
+            }
+            _ => {}
+        }
+    }
+
+    println!("{}", m.get(&'r').unwrap_or(&0))
 }
--- typical90/src/bin/010.rs
+++ typical90/src/bin/010.rs
@@ -3,5 +3,22 @@ use proconio::input;
 use proconio::marker::*;
 
 fn main() {
-    unimplemented!();
+    input!(n: usize, results: [(Usize1, u32); n]);
+
+    let mut acc = vec![vec![0; n + 1]; 2];
+    for (idx, (class, p)) in results.into_iter().enumerate() {
+        let another_class = class ^ 1;
+        acc[class][idx + 1] = acc[class][idx] + p;
+        acc[another_class][idx + 1] = acc[another_class][idx];
+    }
+
+    // println!("{:?}", acc); // debug
+
+    input!(q: usize);
+    for _ in 0..q {
+        input!(l: usize, r: usize);
+        let p1 = acc[0][r] - acc[0][l - 1];
+        let p2 = acc[1][r] - acc[1][l - 1];
+        println!("{} {}", p1, p2);
+    }
 }
