f98e8ea5bff7d3297d2e8dced041eec013de288b
hkdnet <satoko.itse@gmail.com>
2021-07-23 02:16:02 +0000

    011 AC

--- typical90/src/bin/011.rs
+++ typical90/src/bin/011.rs
@@ -2,6 +2,52 @@
 use proconio::input;
 use proconio::marker::*;
 
+use std::collections::BTreeMap;
+
 fn main() {
-    unimplemented!();
+    input!(n: usize, jobs: [(usize, usize, usize); n]);
+    let mut jobs = jobs;
+    jobs.sort_unstable();
+    // println!("{:?}", jobs); // debug
+
+    let mut dp = BTreeMap::new();
+    {
+        let (due, cost, score) = jobs[0];
+        if cost <= due {
+            dp.insert(cost, score);
+        }
+    }
+    // println!("{:?}", dp); // debug
+    for (due, cost, score) in jobs.into_iter().skip(1) {
+        let mut nx = BTreeMap::new();
+        for (&cur_cost, &cur_score) in dp.iter() {
+            let key1 = cost + cur_cost;
+            if key1 <= due {
+                nx.entry(key1)
+                    .and_modify(|e| {
+                        *e = std::cmp::max(cur_score + score, *e);
+                    })
+                    .or_insert(cur_score + score);
+            }
+            nx.entry(cur_cost)
+                .and_modify(|e| {
+                    *e = std::cmp::max(cur_score, *e);
+                })
+                .or_insert(cur_score);
+        }
+        if cost <= due {
+            nx.entry(cost)
+                .and_modify(|e| {
+                    *e = std::cmp::max(*e, score);
+                })
+                .or_insert(score);
+        }
+        std::mem::swap(&mut nx, &mut dp);
+        // println!("{:?}", dp); // debug
+    }
+    let mut ans = 0;
+    for &score in dp.values() {
+        ans = std::cmp::max(ans, score);
+    }
+    println!("{}", ans)
 }
