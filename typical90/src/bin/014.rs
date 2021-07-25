c198cc763b8f22603087a95d59a399478e49f8f2
hkdnet <satoko.itse@gmail.com>
2021-07-25 04:10:32 +0000

    014 AC

--- typical90/src/bin/014.rs
+++ typical90/src/bin/014.rs
@@ -3,5 +3,15 @@ use proconio::input;
 use proconio::marker::*;
 
 fn main() {
-    unimplemented!();
+    input!(n: usize, aa: [i64; n], bb: [i64; n]);
+    let mut aa = aa;
+    let mut bb = bb;
+    aa.sort_unstable();
+    bb.sort_unstable();
+    let ans: i64 = aa
+        .into_iter()
+        .zip(bb.into_iter())
+        .map(|(a, b)| (a - b).abs())
+        .sum();
+    println!("{}", ans)
 }
