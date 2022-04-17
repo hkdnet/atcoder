e7aab40f6f2a23ee251d388b828ae440b3da318b
hkdnet <satoko.itse@gmail.com>
2022-04-17 04:21:09 +0000

    WA

--- typical90/src/bin/017.rs
+++ typical90/src/bin/017.rs
@@ -1,80 +1,162 @@
 #![allow(unused_imports)]
+use std::cmp::Ordering;
+use std::cmp::Reverse;
 use std::collections::BTreeMap;
+use std::ops::AddAssign;
 
 use comp::binary_search::binary_search;
 use proconio::input;
 use proconio::marker::*;
 
-fn main() {
-    // 3 <= N <= 300_000
-    input!(_n: usize, m: usize);
-    // 1 <= l < r <= n
-    input!(lrs: [(usize, usize); m]);
-    // (l1, r1), (l2, r2) について、 l1 < l2 < r1 < r2 が条件な気がする。
-    let mut lrs = lrs;
-    lrs.sort_unstable();
-    let mut m: BTreeMap<usize, Vec<usize>> = BTreeMap::new();
-    for &(l, r) in lrs.iter() {
-        m.entry(l)
-            .and_modify(|v| {
-                v.push(r);
-            })
-            .or_insert_with(|| vec![r]);
-    }
-
-    let mut ans = 0usize;
-    for &(l1, r1) in lrs.iter() {
-        for (&l2, r2s) in m.iter() {
-            ans += f(l1, r1, l2, r2s);
-        }
-    }
-
-    println!("{}", ans);
+pub struct BinaryIndexedTree<T> {
+    size: usize,
+    v: Vec<T>,
 }
-
-fn f(l1: usize, r1: usize, l2: usize, r2s: &[usize]) -> usize {
-    if l1 < l2 && l2 < r1 {
-        let len = r2s.len();
-
-        if let Some(&last) = r2s.last() {
-            if last <= r1 {
-                return 0;
-            }
+impl<T> BinaryIndexedTree<T>
+where
+    T: Default + Clone + Copy + AddAssign + From<u8> + std::fmt::Debug,
+{
+    pub fn new(s: usize) -> BinaryIndexedTree<T> {
+        let size = s + 2;
+        BinaryIndexedTree {
+            size,
+            v: vec![T::default(); size + 2],
         }
-        if len == 1 {
-            return 1;
-        }
+    }
 
-        let idx = binary_search(0, len, |idx| r2s[idx] <= r1);
-        if *r2s.first().unwrap() <= r1 {
-            len - idx - 1
+    pub fn add(&mut self, pos: usize, val: T) {
+        let mut pos = pos;
+        pos += 1;
+        while pos <= self.size {
+            self.v[pos] += val;
+            pos += Self::last_bit(pos);
+        }
+    }
+
+    pub fn sum(&self, pos: usize) -> T {
+        let mut ret = T::from(0u8);
+        let mut pos = pos;
+        pos += 1;
+
+        while pos >= 1 {
+            ret += self.v[pos];
+            pos -= Self::last_bit(pos);
+        }
+        ret
+    }
+
+    // a & (-a)c
+    fn last_bit(a: usize) -> usize {
+        if a == 0 {
+            0
         } else {
-            len - idx
+            1 << a.trailing_zeros()
         }
-    } else {
-        0
     }
 }
 
 #[cfg(test)]
-mod tests {
-    use super::f;
+mod test {
+    use std::fmt::Binary;
 
+    use super::BinaryIndexedTree;
     #[test]
-    fn test_f() {
-        // (10, 20)
-        let l1 = 10;
-        let r1 = 20;
-        assert_eq!(0, f(l1, r1, 15, &[16]));
-        assert_eq!(0, f(l1, r1, 15, &[20]));
-        assert_eq!(1, f(l1, r1, 15, &[25]));
-        assert_eq!(2, f(l1, r1, 15, &[21, 25]));
-        assert_eq!(1, f(l1, r1, 15, &[16, 25]));
-        assert_eq!(1, f(l1, r1, 15, &[16, 20, 25]));
-        assert_eq!(1, f(l1, r1, 15, &[16, 20, 25]));
-        assert_eq!(2, f(l1, r1, 15, &[16, 20, 25, 26]));
+    fn test_bit() {
+        let mut bit = BinaryIndexedTree::<i32>::new(10);
 
-        assert_eq!(0, f(l1, r1, l1, &[16, 20, 25]));
-        assert_eq!(0, f(l1, r1, r1, &[20, 21, 25]));
+        // https://dmoj.ca/problem/ds1
+        let v = vec![4, 8, 4, 5, 6, 3, 2, 2, 8, 1];
+        let change =
+            |mut bit: BinaryIndexedTree<i32>, idx: usize, val: i32| -> BinaryIndexedTree<i32> {
+                let diff = val - v[idx - 1];
+                bit.add(idx - 1, diff);
+                bit
+            };
+        let s = |bit: &BinaryIndexedTree<i32>, l: usize, r: usize| -> i32 {
+            // inclusive
+            let rsum = bit.sum(r - 1);
+            dbg!(rsum);
+            let lsum = if l == 1 { 0 } else { bit.sum(l - 2) };
+            dbg!(lsum);
+            rsum - lsum
+        };
+        for (idx, &val) in v.iter().enumerate() {
+            bit.add(idx, val);
+        }
+
+        bit = change(bit, 7, 6);
+        // Q 7
+        // assert_eq(8, q(7));
+        // S 2 3
+        assert_eq!(12, s(&bit, 2, 3));
+        // S 1 4
+        assert_eq!(21, s(&bit, 1, 4));
+        // C 4 9
+        bit = change(bit, 4, 9);
+        // S 2 3
+        assert_eq!(12, s(&bit, 2, 3));
+        // Q 6
+        // assert_eq!(7, q(6));
+        // C 3 9
+        bit = change(bit, 3, 9);
+        // S 6 7
+        assert_eq!(9, s(&bit, 6, 7));
+        // Q 6
+        // assert_eq!(6, q(6));
     }
+}
+
+fn main() {
+    // 3 <= N <= 300_000
+    input!(n: usize, m: usize);
+    // 1 <= l < r <= n
+    input!(lrs: [(usize, usize); m]);
+
+    let mut lrs = lrs;
+
+    let a1 = {
+        let mut v = vec![0; n + 1];
+        for &(l, r) in lrs.iter() {
+            v[l] += 1;
+            v[r] += 1;
+        }
+        let mut ans = 0;
+        for n in v {
+            if n != 0 {
+                ans += n * (n - 1) / 2;
+            }
+        }
+        ans
+    };
+    let a2 = {
+        let mut ls = vec![0; n + 1];
+        let mut acc_r = vec![0; n + 2];
+        for &(l, r) in lrs.iter() {
+            acc_r[r] += 1;
+            ls[l - 1] += 1;
+        }
+        for idx in 0..n {
+            acc_r[idx + 1] += acc_r[idx];
+        }
+        let mut ans = 0;
+        for idx in 0..n {
+            ans += ls[idx] * acc_r[idx];
+        }
+        ans
+    };
+    lrs.sort_unstable_by_key(|&(_l, r)| r);
+
+    let a3 = {
+        let mut bit = BinaryIndexedTree::<usize>::new(n + 2);
+        let mut ans = 0;
+        for &(l, r) in lrs.iter() {
+            let ret = bit.sum(r) - bit.sum(l);
+            ans += ret;
+            bit.add(l, 1);
+        }
+        ans
+    };
+    let total = m * (m - 1) / 2;
+
+    println!("{}", total - a1 - a2 - a3);
 }
