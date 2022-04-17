7f69be66b9ce2f332fe45abe34da966761bf5ba6
hkdnet <satoko.itse@gmail.com>
2022-04-17 05:00:27 +0000

    018 AC

--- typical90/src/bin/018.rs
+++ typical90/src/bin/018.rs
@@ -1,7 +1,31 @@
 #![allow(unused_imports)]
+use std::f64::consts::PI;
+use std::f64::consts::TAU;
+
 use proconio::input;
 use proconio::marker::*;
 
 fn main() {
-    unimplemented!();
+    input!(tt: f64, l: f64, x: f64, y: f64);
+    input!(q: usize);
+    input!(ts: [f64; q]);
+
+    let hl = l / 2.;
+
+    for t in ts {
+        let rad = t / tt * TAU;
+        let cos = rad.cos();
+        let sin = rad.sin();
+        let height = hl - hl * cos;
+        // statue = (x, y, 0)
+        // ferris = (0, -L/2(sin), L/2(1-cos))
+        let len = {
+            let xx = x.powf(2.);
+            let yy = (y + sin * hl).powf(2.);
+            (xx + yy).sqrt()
+        };
+        let ans_rad = (height / len).atan();
+
+        println!("{}", ans_rad * 360. / TAU);
+    }
 }
