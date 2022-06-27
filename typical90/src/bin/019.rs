b811add092b71fa98a9d2a0b3769596247b12d51
hkdnet <satoko.itse@gmail.com>
2022-06-27 12:15:52 +0000

    WA

--- typical90/src/bin/019.rs
+++ typical90/src/bin/019.rs
@@ -3,5 +3,13 @@ use proconio::input;
 use proconio::marker::*;
 
 fn main() {
-    unimplemented!();
+    input!(n: usize);
+    input!(aa: [i32; 2 * n]);
+    let mut idx = 0;
+    let mut ans = 0;
+    while idx < aa.len() {
+        ans += aa[idx + 1] - aa[idx];
+        idx += 2;
+    }
+    println!("{}", ans.abs());
 }
