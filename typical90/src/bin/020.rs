13d79f33ca635eef7100e5780b65d5f008a229d9
hkdnet <satoko.itse@gmail.com>
2022-09-11 14:26:18 +0900

    AC 020

--- typical90/src/bin/020.rs
+++ typical90/src/bin/020.rs
@@ -3,5 +3,7 @@ use proconio::input;
 use proconio::marker::*;
 
 fn main() {
-    unimplemented!();
+    input!(a: u64, b: u32, c: u64);
+    let ans = if a < c.pow(b) { "Yes" } else { "No" };
+    println!("{}", ans);
 }
