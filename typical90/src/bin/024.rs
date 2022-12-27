1791189b91d77b68d7131de73c7e18d81de69c0b
hkdnet <satoko.itse@gmail.com>
2022-12-27 22:55:32 +0900

    024 AC

--- typical90/src/bin/024.rs
+++ typical90/src/bin/024.rs
@@ -3,5 +3,18 @@ use proconio::input;
 use proconio::marker::*;
 
 fn main() {
-    unimplemented!();
+    input!(n: usize, k: i64);
+    input!(aa: [i64; n]);
+    input!(bb: [i64; n]);
+
+    let diffs = aa.iter().zip(bb.iter()).map(|(&a, &b)| (a - b).abs());
+    let mut sum = 0;
+    for d in diffs {
+        sum += d;
+    }
+    if k >= sum && k % 2 == sum % 2 {
+        println!("Yes")
+    } else {
+        println!("No")
+    }
 }
