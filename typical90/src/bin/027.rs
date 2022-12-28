ca35871d5a68d50f832d554c546d6c9bf0726635
hkdnet <satoko.itse@gmail.com>
2022-12-28 14:36:11 +0900

    027 AC

--- typical90/src/bin/027.rs
+++ typical90/src/bin/027.rs
@@ -3,5 +3,13 @@ use proconio::input;
 use proconio::marker::*;
 
 fn main() {
-    unimplemented!();
+    input!(n: usize);
+    let mut m = std::collections::BTreeSet::new();
+    for i in 0..n {
+        input!(s: String);
+        if !m.contains(&s) {
+            println!("{}", i + 1);
+            m.insert(s);
+        }
+    }
 }
