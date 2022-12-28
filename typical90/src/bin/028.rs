d5114e721593f3f5170af8ff6ea7844e9a2baa4e
hkdnet <satoko.itse@gmail.com>
2022-12-28 14:55:12 +0900

    028 AC

--- typical90/src/bin/028.rs
+++ typical90/src/bin/028.rs
@@ -3,5 +3,42 @@ use proconio::input;
 use proconio::marker::*;
 
 fn main() {
-    unimplemented!();
+    input!(n: usize);
+    // imos
+    let mut xy = vec![vec![0; 1001]; 1001];
+    for _ in 0..n {
+        input!(lx1: usize, ly1: usize, lx2: usize, ly2: usize);
+
+        xy[lx1][ly1] += 1;
+        xy[lx1][ly2] -= 1;
+        xy[lx2][ly1] -= 1;
+        xy[lx2][ly2] += 1;
+    }
+
+    for x in 0..xy.len() {
+        for y in 0..xy[x].len() - 1 {
+            xy[x][y + 1] += xy[x][y];
+        }
+    }
+
+    for y in 0..xy[0].len() {
+        for x in 0..xy.len() - 1 {
+            xy[x + 1][y] += xy[x][y];
+        }
+    }
+
+    let mut cnt = std::collections::HashMap::<i32, i32>::new();
+    for r in xy {
+        for v in r {
+            if v != 0 {
+                *cnt.entry(v).or_default() += 1
+            }
+        }
+    }
+
+    for i in 1..=n {
+        let idx = i as i32;
+        let c = cnt.entry(idx).or_default();
+        println!("{}", c);
+    }
 }
