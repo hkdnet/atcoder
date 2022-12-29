766d65c3add53f09b35f5b295e1dac693ce12303
hkdnet <satoko.itse@gmail.com>
2022-12-29 13:42:06 +0900

    030 AC

--- typical90/src/bin/030.rs
+++ typical90/src/bin/030.rs
@@ -3,5 +3,32 @@ use proconio::input;
 use proconio::marker::*;
 
 fn main() {
-    unimplemented!();
+    input!(n: usize, k: i32);
+    let mut c = vec![0; n + 1];
+
+    let mut osak = vec![0; n + 1];
+    osak[1] = 1;
+    for i in 2..=n {
+        if osak[i] != 0 {
+            continue;
+        }
+        let mut idx = i;
+        while idx < osak.len() {
+            osak[idx] = idx / i;
+            idx += i;
+        }
+    }
+
+    for i in 2..=n {
+        if osak[i] == 1 {
+            let mut idx = i;
+            while idx < c.len() {
+                c[idx] += 1;
+                idx += i;
+            }
+        }
+    }
+
+    let ans = c.into_iter().filter(|&v| v >= k).count();
+    println!("{}", ans)
 }
