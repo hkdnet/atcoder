915d2c8bb07a15a58e7ae10f8ea90d9fb3b3515f
hkdnet <satoko.itse@gmail.com>
2022-12-29 14:24:35 +0900

    033 AC

--- typical90/src/bin/033.rs
+++ typical90/src/bin/033.rs
@@ -3,5 +3,14 @@ use proconio::input;
 use proconio::marker::*;
 
 fn main() {
-    unimplemented!();
+    input!(h: usize, w: usize);
+
+    let ans = if h == 1 || w == 1 {
+        std::cmp::max(h, w)
+    } else {
+        let hh = (h + 1) / 2;
+        let ww = (w + 1) / 2;
+        hh * ww
+    };
+    println!("{}", ans);
 }
