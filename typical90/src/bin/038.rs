bff5c0c2729db785eb14e170833e37618dfc2770
hkdnet <satoko.itse@gmail.com>
2023-01-06 11:08:16 +0900

    037 AC

--- typical90/src/bin/038.rs
+++ typical90/src/bin/038.rs
@@ -3,5 +3,20 @@ use proconio::input;
 use proconio::marker::*;
 
 fn main() {
-    unimplemented!();
+    input!(a: u128, b: u128);
+    let g = gcd(a, b);
+    let ans = a / g * b;
+    if ans > 1_000_000_000_000_000_000 {
+        println!("Large")
+    } else {
+        println!("{}", ans);
+    }
+}
+
+fn gcd(a: u128, b: u128) -> u128 {
+    if b == 0 {
+        a
+    } else {
+        gcd(b, a % b)
+    }
 }
