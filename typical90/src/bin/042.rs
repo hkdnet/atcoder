f56515c6742d3b25fcbd7df5b6dc63095f52c60f
hkdnet <satoko.itse@gmail.com>
2023-01-26 12:12:35 +0900

    042 AC

--- typical90/src/bin/042.rs
+++ typical90/src/bin/042.rs
@@ -2,6 +2,22 @@
 use proconio::input;
 use proconio::marker::*;
 
+const MOD: u64 = 1_000_000_007;
+
 fn main() {
-    unimplemented!();
+    input!(k: usize);
+    if k == 0 || k % 9 != 0 {
+        println!("{}", 0);
+        return;
+    }
+    let mut dp = vec![0; k + 10];
+    dp[0] = 1;
+
+    for i in 0..k {
+        for j in 1..=9 {
+            dp[i + j] += dp[i];
+            dp[i + j] %= MOD;
+        }
+    }
+    println!("{}", dp[k]);
 }
