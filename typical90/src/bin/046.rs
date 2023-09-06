43503a61c4046ea734d0f6c252fde752360e8831
hkdnet <satoko.itse@gmail.com>
2023-09-06 16:37:48 +0900

    046

--- typical90/src/bin/046.rs
+++ typical90/src/bin/046.rs
@@ -4,5 +4,33 @@ use proconio::marker::*;
 
 #[allow(non_snake_case)]
 fn main() {
-    unimplemented!();
+    input!(N: usize);
+    input!(aa: [i32; N]);
+    input!(bb: [i32; N]);
+    input!(cc: [i32; N]);
+    let to_c = |v: &[i32]| {
+        let mut ret = std::collections::BTreeMap::<i32, usize>::new();
+        for i in v {
+            let k = i % 46;
+            ret.entry(k).and_modify(|a| *a += 1).or_insert(1);
+        }
+
+        ret
+    };
+    let ca = to_c(&aa);
+    let cb = to_c(&bb);
+    let cc = to_c(&cc);
+
+    let mut ans = 0;
+    for (&a, &c1) in ca.iter() {
+        for (&b, &c2) in cb.iter() {
+            let tmp = (a + b) % 46;
+            let c = (46 - tmp) % 46;
+            if let Some(c3) = cc.get(&c) {
+                ans += c1 * c2 * c3;
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
