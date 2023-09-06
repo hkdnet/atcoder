34b1e221c346e990ce4fea343c0b98efea748545
hkdnet <satoko.itse@gmail.com>
2023-09-06 18:26:56 +0900

    050

--- typical90/src/bin/050.rs
+++ typical90/src/bin/050.rs
@@ -4,5 +4,16 @@ use proconio::marker::*;
 
 #[allow(non_snake_case)]
 fn main() {
-    unimplemented!();
+    let MOD = 1_000_000_007;
+    input!(N: usize, L: usize);
+    let mut v = vec![0; 200001];
+    v[0] = 1;
+    for i in 0..=N {
+        v[i + 1] += v[i];
+        v[i + 1] %= MOD;
+        v[i + L] += v[i];
+        v[i + L] %= MOD;
+    }
+
+    println!("{}", v[N]);
 }
