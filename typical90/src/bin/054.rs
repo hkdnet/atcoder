97e0e448640bdff102e00785d79825ea1bcffecc
hkdnet <satoko.itse@gmail.com>
2023-09-14 12:07:33 +0900

    typical90 054

--- typical90/src/bin/054.rs
+++ typical90/src/bin/054.rs
@@ -1,8 +1,38 @@
 #![allow(unused_imports)]
+use std::collections::BTreeSet;
+use std::collections::VecDeque;
+
 use proconio::input;
 use proconio::marker::*;
 
 #[allow(non_snake_case)]
 fn main() {
-    unimplemented!();
+    input!(N: usize, M: usize);
+    let mut G = vec![BTreeSet::new(); N + M];
+    for i in 0..M {
+        input!(K: usize);
+        input!(p: [Usize1; K]);
+        let paper_node = N + i;
+        for r in p {
+            G[r].insert(paper_node);
+            G[paper_node].insert(r);
+        }
+    }
+
+    let mut t = vec![-2; N + M];
+    t[0] = 0;
+    let mut q = VecDeque::new();
+    q.push_back(0);
+    while let Some(node) = q.pop_front() {
+        for &next in G[node].iter() {
+            if t[next] == -2 {
+                t[next] = t[node] + 1;
+                q.push_back(next);
+            }
+        }
+    }
+
+    for i in 0..N {
+        println!("{}", t[i] / 2);
+    }
 }
