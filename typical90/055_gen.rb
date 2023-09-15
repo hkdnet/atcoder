3a17c99204c1c1ad5fc0033236ec5604c4a5eecc
hkdnet <satoko.itse@gmail.com>
2023-09-15 09:22:29 +0900

    055

---
+++ typical90/055_gen.rb
@@ -0,0 +1,6 @@
+N = 100
+P = 99999999
+Q = 88888888
+aa = N.times.map { rand(123456) }
+puts "#{N} #{P} #{Q}"
+puts aa.join(" ")
--- typical90/src/bin/055.rs
+++ typical90/src/bin/055.rs
@@ -4,5 +4,21 @@ use proconio::marker::*;
 
 #[allow(non_snake_case)]
 fn main() {
-    unimplemented!();
+    input!(N: usize, P: u128, Q: u128);
+    input!(aa: [u128; N]);
+    let mut ans = 0u64;
+    for a1 in 0..N {
+        for a2 in a1 + 1..N {
+            for a3 in a2 + 1..N {
+                for a4 in a3 + 1..N {
+                    for a5 in a4 + 1..N {
+                        if aa[a1] % P * aa[a2] % P * aa[a3] % P * aa[a4] % P * aa[a5] % P == Q {
+                            ans += 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    println!("{}", ans);
 }
