6f740cc8d388e9558dd1f0a598e114c82b03eccd
hkdnet <satoko.itse@gmail.com>
2023-10-14 00:01:22 +0900

    tmpl

---
+++ abc323/a/ans.rb
@@ -0,0 +1,12 @@
+#/usr/bin/env ruby
+
+def geti
+  gets.chomp.to_i
+end
+
+def getis
+  gets.chomp.split(" ").map(&:to_i)
+end
+
+f = gets.chomp.split("").each_slice(2).map(&:last).all? { |e| e == ?0 }
+puts(f ? "Yes" : "No")
