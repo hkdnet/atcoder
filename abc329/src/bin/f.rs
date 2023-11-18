21b5bb286a0a9f4008336c9329c23bf32eb02dea
hkdnet <satoko.itse@gmail.com>
2023-11-19 00:22:24 +0900

    F AC

--- abc329/src/bin/f.rs
+++ abc329/src/bin/f.rs
@@ -1,8 +1,32 @@
 #![allow(unused_imports)]
 #![allow(non_snake_case)]
+use std::collections::BTreeSet;
+
 use proconio::input;
 use proconio::marker::*;
 
 fn main() {
-    unimplemented!();
+    input!(N: usize, Q: usize);
+    input!(cc: [usize; N]);
+    let mut v = vec![BTreeSet::new(); N];
+    for (i, c) in cc.into_iter().enumerate() {
+        v[i].insert(c);
+    }
+    for _ in 0..Q {
+        input!(a: Usize1, b: Usize1);
+        let mut a = a;
+        let mut b = b;
+        let mut swap = false;
+        if v[a].len() > v[b].len() {
+            std::mem::swap(&mut a, &mut b);
+            swap = true;
+        }
+        while let Some(i) = v[a].pop_first() {
+            v[b].insert(i);
+        }
+        println!("{}", v[b].len());
+        if swap {
+            v.swap(a, b);
+        }
+    }
 }
