3338c0dc13403475a6a8a964780281b9102dad09
hkdnet <satoko.itse@gmail.com>
2024-01-21 01:32:11 +0900

    E AC

--- abc335/src/bin/e.rs
+++ abc335/src/bin/e.rs
@@ -2,6 +2,7 @@
 #![allow(non_snake_case)]
 #![allow(unused_macros)]
 use std::collections::BTreeSet;
+use std::collections::BinaryHeap;
 use std::collections::HashMap;
 use std::collections::VecDeque;
 
@@ -59,75 +60,62 @@ fn main() {
     input!(a: [u32; N]);
     input!(es: [(Usize1, Usize1); M]);
     let mut uf = UnionFind::new(N);
-    let m = {
-        let mut m = HashMap::<usize, BTreeSet<usize>>::new();
-        for (u, v) in es {
-            let mut u = u;
-            let mut v = v;
-            if a[u] > a[v] {
-                std::mem::swap(&mut u, &mut v);
-            }
-            if a[u] == a[v] {
-                uf.unite(u, v);
-            } else {
-                m.entry(u)
-                    .and_modify(|e| {
-                        e.insert(v);
-                    })
-                    .or_insert_with(|| BTreeSet::from([v]));
-            }
+    for &(u, v) in es.iter() {
+        if a[u] == a[v] {
+            uf.unite(u, v);
         }
-        let mut merger = BTreeMap::<usize, BTreeSet<usize>>::new();
-        for i in 0..N {
-            let r = uf.root(i);
-            merger
-                .entry(r)
+    }
+    let node_per_point = {
+        let mut m = BTreeMap::<u32, BTreeSet<usize>>::new();
+        for (i, &nove_val) in a.iter().enumerate() {
+            let i = uf.root(i);
+            m.entry(nove_val)
                 .and_modify(|e| {
                     e.insert(i);
                 })
                 .or_insert_with(|| BTreeSet::from([i]));
         }
-        for (_, s) in merger {
-            if s.len() < 2 {
+        m
+    };
+    let m = {
+        let mut m = HashMap::<usize, BTreeSet<usize>>::new();
+        for (u, v) in es {
+            let mut u = uf.root(u);
+            let mut v = uf.root(v);
+            if u == v {
                 continue;
             }
-            let mut se = BTreeSet::new();
-            for &i in s.iter() {
-                if let Some(ss) = m.get(&i) {
-                    se.append(&mut ss.clone());
-                }
+            if a[u] > a[v] {
+                std::mem::swap(&mut u, &mut v);
             }
-            for &i in s.iter() {
-                m.insert(i, se.clone());
-            }
+            m.entry(u)
+                .and_modify(|e| {
+                    e.insert(v);
+                })
+                .or_insert_with(|| BTreeSet::from([v]));
         }
         m
     };
-    let mut q = VecDeque::from([(0, 1)]);
-    let mut cnt = vec![0u64; N];
-    cnt[0] = 1;
-    while let Some((cur, expected_cnt)) = q.pop_front() {
-        if cur == N - 1 {
-            continue;
-        }
-        if cnt[cur] != expected_cnt {
-            continue;
-        }
+    let start = uf.root(0);
+    let mut dp = vec![0u64; N];
+    dp[start] = 1;
+    let goal = uf.root(N - 1);
+    for (&_, v) in node_per_point.iter() {
+        for &cur in v.iter() {
+            let cur_v = dp[cur];
+            if cur_v == 0 {
+                continue;
+            }
 
-        let cur_v = a[cur];
-        if let Some(dests) = m.get(&cur) {
-            for &d in dests.iter() {
-                let nx_v = if a[d] == cur_v {
-                    cnt[cur]
-                } else {
-                    cnt[cur] + 1
-                };
-                if cnt[d] < nx_v {
-                    cnt[d] = nx_v;
-                    q.push_back((d, nx_v));
+            if let Some(dests) = m.get(&cur) {
+                for &d in dests.iter() {
+                    let nx_v = cur_v + 1;
+                    if dp[d] < nx_v {
+                        dp[d] = nx_v;
+                    }
                 }
             }
         }
     }
-    println!("{}", cnt[N - 1]);
+    println!("{}", dp[goal]);
 }
