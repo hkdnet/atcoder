ff161144d3545f11ac761bb3c834ca16501bb55d
hkdnet <satoko.itse@gmail.com>
2024-02-17 20:48:27 +0900

    339

--- abc339/src/bin/e.rs
+++ abc339/src/bin/e.rs
@@ -1,6 +1,10 @@
 #![allow(unused_imports)]
 #![allow(non_snake_case)]
 #![allow(unused_macros)]
+use ac_library::Additive;
+use ac_library::LazySegtree;
+use ac_library::MapMonoid;
+use ac_library::Max;
 use proconio::input;
 use proconio::marker::*;
 
@@ -10,7 +14,35 @@ macro_rules! debug {
         eprintln!(concat!($("| ", stringify!($a), "={:?} "),*, "|"), $(&$a),*);
     };
 }
+struct MaxAdd;
+impl MapMonoid for MaxAdd {
+    type M = Max<u64>;
+    type F = u64;
 
+    fn identity_map() -> Self::F {
+        0
+    }
+
+    fn mapping(&f: &u64, &x: &u64) -> u64 {
+        f + x
+    }
+
+    fn composition(&f: &u64, &g: &u64) -> u64 {
+        f + g
+    }
+}
 fn main() {
-    unimplemented!();
+    input!(N: usize, D: usize);
+    input!(A: [usize; N]);
+
+    let mut seg = LazySegtree::<MaxAdd>::new(1_000_002);
+    for &a in A.iter() {
+        let l = if a < D { 0 } else { a - D };
+        let r = a + D;
+        let m = seg.prod(l..=r);
+        seg.set(a, m + 1);
+    }
+
+    let ans = seg.all_prod();
+    println!("{}", ans);
 }
--- abc339/src/bin/f.rs
+++ abc339/src/bin/f.rs
@@ -1,6 +1,11 @@
 #![allow(unused_imports)]
 #![allow(non_snake_case)]
 #![allow(unused_macros)]
+use std::collections::BTreeMap;
+use std::collections::BTreeSet;
+use std::mem::swap;
+
+use itertools::Itertools;
 use proconio::input;
 use proconio::marker::*;
 
@@ -10,7 +15,72 @@ macro_rules! debug {
         eprintln!(concat!($("| ", stringify!($a), "={:?} "),*, "|"), $(&$a),*);
     };
 }
+use rand::prelude;
+
+fn random_big_values() -> Vec<u64> {
+    (0..6)
+        .map(|_| {
+            let v = rand::random::<u16>() as u64;
+
+            1_000_000_000 + v
+        })
+        .collect()
+}
 
 fn main() {
-    unimplemented!();
+    input!(N: usize);
+    input!(a: [Bytes; N]);
+    let mut ans = BTreeSet::new();
+    for i in 0..N {
+        for j in 0..N {
+            for k in 0..N {
+                ans.insert((i, j, k));
+            }
+        }
+    }
+    let bv = random_big_values();
+    let mut vv = vec![vec![0; N]; bv.len()];
+    for (mi, &m) in bv.iter().enumerate() {
+        for (i, cs) in a.iter().enumerate() {
+            let mut b = 0;
+            for u in cs {
+                b = (b * 10 + (u - b'0') as u64) % m;
+            }
+            vv[mi][i] = b;
+        }
+    }
+
+    for m in bv {
+        let mut h = BTreeMap::new();
+        let mut v = vec![0; N];
+        for (i, cs) in a.iter().enumerate() {
+            let mut b = 0;
+            for u in cs {
+                b = (b * 10 + (u - b'0') as u64) % m;
+            }
+            v[i] = b;
+            h.entry(b).or_insert(BTreeSet::new()).insert(i);
+        }
+
+        let mut tmp = BTreeSet::new();
+        for i in 0..N {
+            for j in 0..N {
+                let k = (v[i] * v[j]) % m;
+                if let Some(s) = h.get(&k) {
+                    for &k in s.iter() {
+                        tmp.insert((i, j, k));
+                    }
+                }
+            }
+        }
+
+        let mut new_ans = BTreeSet::new();
+        for i in tmp {
+            if ans.contains(&i) {
+                new_ans.insert(i);
+            }
+        }
+        swap(&mut ans, &mut new_ans);
+    }
+    println!("{}", ans.len());
 }
