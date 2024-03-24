4478b812b15afa64c2cfadbce4c05b14a4245d0e
hkdnet <satoko.itse@gmail.com>
2024-03-24 23:32:55 +0900

    tessoku: a32

--- tessoku-book/a32/ans.rb
+++ tessoku-book/a32/ans.rb
@@ -5,3 +5,23 @@ end
 def getis
   gets.chomp.split(" ").map(&:to_i)
 end
+
+N, A, B = getis
+g = [] # true で勝ち, false で負け
+(N+1).times do |i|
+  s =
+    if i >= A && !g[i-A] # A を選んだら負けに遷移できる
+      true
+    elsif i >= B && !g[i-B] # B を選んだら負けに遷移できる
+      true
+    else
+      false
+    end
+  g[i] = s
+end
+
+if g[N]
+  puts "First"
+else
+  puts "Second"
+end
