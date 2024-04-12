6d795e433fccc0f20dc1d7de35f018d8396cd058
hkdnet <satoko.itse@gmail.com>
2024-04-12 23:31:52 +0900

    update template

--- rb-tmpl/ans.rb
+++ rb-tmpl/ans.rb
@@ -1,7 +1,4 @@
-def geti
-  gets.chomp.to_i
-end
-
-def getis
-  gets.chomp.split(" ").map(&:to_i)
-end
+def getl = gets.chomp
+def geti = getl.to_i
+def getis = getl.split(" ").map(&:to_i)
+def getis1 = getl.split(" ").map { |e| e.to_i - 1 }
